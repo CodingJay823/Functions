60f4b814f91dcee4b03016af2494814bffa35fd2
CodingJay823 <67976024+CodingJay823@users.noreply.github.com>
2020-07-20 14:15:56 -0500

    Finished

---
+++ Jada_H_FunctionExercise.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+// Create a function that adds two numbers
+func add(num1: Int, num2: Int) -> Int{
+    let adding = num1 + num2
+    return (adding)
+}
+
+print(add(num1: 5, num2: 5))
